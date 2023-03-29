4471dd7f8f299499f245d2760563e263f77bc469
TonyxRazzor <tonyxrazzor@yandex.ru>
2023-03-29 16:00:45 +0300

    Тест прокта №7: ver 1.1 (insort)

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,7 +1,6 @@
+from django.urls import include, path
 from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
-
-from django.urls import include, path
 
 from .views import CommentViewSet, GroupViewSet, PostViewSet
 
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,9 +1,7 @@
-from rest_framework import viewsets
-from rest_framework import permissions
-
 from django.shortcuts import get_object_or_404
-
 from posts.models import Group, Post
+from rest_framework import permissions, viewsets
+
 from .serializers import CommentSerializer, GroupSerializer, PostSerializer
 
 
